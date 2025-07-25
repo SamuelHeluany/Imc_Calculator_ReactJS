4a1f3d65da99350d052e147c0cf1fb000167e34d
Samuel Heluany <samuelheluany@hotmail.com>
2025-07-25 09:49:47 -0300

    Clear form function

--- src/components/Calculator/ImcCalc.jsx
+++ src/components/Calculator/ImcCalc.jsx
@@ -1,25 +1,52 @@
+import { useState } from 'react'
 import Button from '../Button/Button'
 import './ImcCalc.css'
+
 const ImcCalc = () => {
-  return <div id="calc-container">
+    const [height, setHeight] = useState('')
+    const [weight, setWeight] = useState('')
+
+    const clearForm = (e) => {
+        e.preventDefault()
+        setHeight('')
+        setWeight('')
+    }
+
+  return (
+  <div id="calc-container">
     <h2>Calculadora de IMC</h2>
     <form id="imc-form">
         <div id="form-inputs">
             <div id="form-control">
                 <label htmlFor="height">Altura:</label>
-                <input type="text" name="height" id="height" placeholder='Exemplo 1,75'/>
+                <input 
+                type="number" 
+                name="height" 
+                id="height" 
+                placeholder='Exemplo 1,75'
+                onChange={(e) => setHeight(e.target.value)}
+                value={height}
+                />
             </div>
             <div id="form-control">
                 <label htmlFor="weight">Peso:</label>
-                <input type="text" name="weight" id="weight" placeholder='Exemplo 71,8'/>
+                <input 
+                type="number" 
+                name="weight" 
+                id="weight" 
+                placeholder='Exemplo 71,8'
+                onChange={(e) => setWeight(e.target.value)}
+                value={weight}
+                />
             </div>
             <div id="action-control">
                 <Button id='calc-btn' text='Calcular'/>
-                <Button id='clear-btn' text='Limpar'/>
+                <Button id='clear-btn' text='Limpar' action={clearForm}/>
             </div>
         </div>
     </form>
   </div>
+  )
 }
 
 export default ImcCalc
