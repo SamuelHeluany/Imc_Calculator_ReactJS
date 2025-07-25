0cd378e8bf357dba812b571227f2c69e3756223e
Samuel Heluany <samuelheluany@hotmail.com>
2025-07-25 09:07:42 -0300

    Create data folder with IMC data informations

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -5,16 +5,16 @@ const ImcCalc = () => {
     <form className="imc-form">
         <div className="form-inputs">
             <div className="form-control">
-                <label htmlFor="height">Height:</label>
+                <label htmlFor="height">Altura:</label>
                 <input type="text" name="height" id="height" placeholder='Exemplo 1,75'/>
             </div>
             <div className="form-control">
-                <label htmlFor="weight">Weight:</label>
+                <label htmlFor="weight">Peso:</label>
                 <input type="text" name="weight" id="weight" placeholder='Exemplo 71,8'/>
             </div>
             <div className="action-control">
-                <button>Calculate</button>
-                <button>Clear</button>
+                <button>Calcular</button>
+                <button>Limpar</button>
             </div>
         </div>
     </form>
